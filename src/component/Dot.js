f6d8edeac97edfa986314687451d3eef553ba027
mofeomotosho8@gmail.com <mofeomotosho8@gmail.com>
2022-06-01 22:48:21 -0300

    setting up project

---
+++ src/component/Dot.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const Dots = () => {
+    return(
+        <View><Text>{'MOFE'}</Text></View>
+    );
+}
+
+export default Dots
